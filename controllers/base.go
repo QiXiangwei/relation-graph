c6aa80a9e6e73144fd9dc435c1bfce68e0bc2b59
qixiangwe <m18745863572@163.com>
2020-07-01 17:56:35 +0800

    create base controller

---
+++ controllers/base.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+)
+
+type MvcHandle func(*mvc.Application)
+type ControllerConstructor func() ([]iris.Handler, interface{})
+
+type BaseController struct {
+	Ctx *iris.Context
+}
+
+var (
+	Controllers map[string]MvcHandle
+)
+
+func RegisterController(path string, constructor ControllerConstructor) {
+	var (
+		handle MvcHandle
+	)
+	handle = func(app *mvc.Application) {
+		beforeMiddleWareHandlers, controller := constructor()
+		app.Router.Use(beforeMiddleWareHandlers...)
+		app.Handle(controller)
+	}
+	if Controllers == nil {
+		Controllers = make(map[string]MvcHandle)
+	}
+	Controllers[path] = handle
+}
